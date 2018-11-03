77731db12d5d6df3c68d6bb2fd100754d8ac2ee7
Jim Shi <shiyilin2008@gmail.com>
2018-11-03 15:51:50 +0800

    1. This script now works down to the index file json level. 2. This script now reads configuration from file "config.py" rather than asking the user about them every time.

---
+++ config.py
@@ -0,0 +1,17 @@
+# Minecraft Music Extractor configuration.
+
+
+# 1. ".minecraft" (or "minecraft" on macOS) folder path.
+# If `None` or an empty string, will use the default Minecraft path.
+minecraftFolderPath = None
+
+
+# 2. Music and sound effects export folder path.
+# If `None` or an empty string, all music and sound effects will be saved in the same folder of "main.py".
+exportFolderPath = None
+
+
+# 3. Minecraft assets index version.
+# If `None` or an empty string, this script will attempt to locate and extract music and sound effects from the LATEST Minecraft version on your computer.
+assetsIndexVersion = None
+# assetsIndexVersion = "1.12"
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import platform
 import os
 import sys
 import distutils.version
+import json
+
+import config
 
 
 # MARK: Helper functions.
@@ -32,50 +35,92 @@ print("Minecraft Music Extractor by MacJim")
 print("This script extracts music & sound effects from your local Minecraft game folder.")
 print("For more information on your Minecraft game folder, please refer to this wiki page:")
 print("https://minecraft.gamepedia.com/.minecraft")
-print("Please run Minecraft at least once before running this script.")
+print("Please run Minecraft at least once before running this script.\n")
 
 
 # MARK: Preparations.
 # 1. Get system platform.
 systemPlatform = platform.system()    # Linux, Darwin or Windows
 
-# 2. Try to obtain ".minecraft" folder path automatically.
-minecraftFolderPath = ""
+# 2. Obtain ".minecraft" folder path.
+minecraftFolderPath = config.minecraftFolderPath
 indexesFolderPath = ""
 objectsFolderPath = ""
 
-# 2-1. Default ".minecraft" folder path.
-if (systemPlatform == "Windows"):
-    appdataPath = os.getenv("APPDATA")
-    minecraftFolderPath = os.path.join(appdataPath, ".minecraft")
-else:
-    homeDirectoryPath = os.path.expanduser("~")
-    if (systemPlatform == "Darwin"):
-        minecraftFolderPath = os.path.join(homeDirectoryPath, "Library/Application Support/minecraft")    # No dot on macOS.
-    elif (systemPlatform == "Linux"):
-        minecraftFolderPath = os.path.join(homeDirectoryPath, ".minecraft")
+if ((minecraftFolderPath is None) or (minecraftFolderPath == "")):
+    # 2-1. Default ".minecraft" folder path.
+    if (systemPlatform == "Windows"):
+        appdataPath = os.getenv("APPDATA")
+        minecraftFolderPath = os.path.join(appdataPath, ".minecraft")
+    else:
+        homeDirectoryPath = os.path.expanduser("~")
+        if (systemPlatform == "Darwin"):
+            minecraftFolderPath = os.path.join(homeDirectoryPath, "Library/Application Support/minecraft")    # No dot on macOS.
+        elif (systemPlatform == "Linux"):
+            minecraftFolderPath = os.path.join(homeDirectoryPath, ".minecraft")
 
 # 2-2. Makes sure that the default ".minecraft" folder and its "assets/indexes" & "assets/objects" subfolders exist.
-shouldEnterMinecraftFolderManually = False
-while True:
-    if (os.path.isdir(minecraftFolderPath)):
-        indexesFolderPath = os.path.join(minecraftFolderPath, "assets/indexes")
-        objectsFolderPath = os.path.join(minecraftFolderPath, "assets/objects")
+if (os.path.isdir(minecraftFolderPath)):
+    print("Minecraft folder found:", minecraftFolderPath)
 
-        if ((not os.path.isdir(indexesFolderPath)) or (not os.path.isdir(objectsFolderPath))):
-            shouldEnterMinecraftFolderManually = True
+    indexesFolderPath = os.path.join(minecraftFolderPath, "assets/indexes")
+    objectsFolderPath = os.path.join(minecraftFolderPath, "assets/objects")
+
+    if (os.path.isdir(indexesFolderPath)):
+        print("Indexes folder found:", indexesFolderPath)
     else:
-        shouldEnterMinecraftFolderManually = True
+        print("Indexes folder not found!")
+        print("Please run Minecraft at least once before running this script!")
+        exit(1)
 
-    if (shouldEnterMinecraftFolderManually):
-        minecraftFolderPath = input("Unable to find your minecraft folder, please enter its path manually: ")
+    if (os.path.isdir(objectsFolderPath)):
+        print("Objects folder found:", objectsFolderPath)
     else:
-        print("Minecraft folder found:", minecraftFolderPath)
-        break
+        print("Objects folder not found!")
+        print("Please run Minecraft at least once before running this script!")
+        exit(1)
 
-# TODO: 3. Parse "assets/indexes/1.x.json" file
-allVersions = []
-distutils.version.LooseVersion
+else:
+    print("Minecraft game folder not found, please enter its path manually in \"config.py\".")
+    exit(1)
+
+# 3. Obtain export folder path.
+exportFolderPath = config.exportFolderPath
+if ((exportFolderPath is None) or (exportFolderPath == "")):
+    exportFolderPath = os.path.dirname(os.path.abspath(__file__))
+
+# 4. Parse "assets/indexes/1.x.json" file
+assetsIndexVersion = config.assetsIndexVersion
+if ((assetsIndexVersion is None) or (assetsIndexVersion == "")):
+    # 4-1. Attempt to find the LATEST Minecraft asset version.
+    allVersions = []
+
+    allFilenamesInIndexesFolder = os.listdir(indexesFolderPath)
+    for aFilename in allFilenamesInIndexesFolder:
+        if (aFilename.endswith(".json")):
+            allVersions.append(aFilename[0:-5])
+
+    if (len(allVersions) <= 0):
+        print("No asset index found!")
+        print("Please run Minecraft at least once before running this script!")
+        exit(1)
+    else:
+        assetsIndexVersion = allVersions[1]
+        for currentVersion in allVersions:
+            if (distutils.version.LooseVersion(assetsIndexVersion) < distutils.version.LooseVersion(currentVersion)):
+                assetsIndexVersion = currentVersion
+        print("Latest assets index found:", assetsIndexVersion)
+
+# 4-2. Make sure that the index file exists.
+assetsIndexFilePath = os.path.join(indexesFolderPath, assetsIndexVersion + ".json")
+if (not os.path.isfile(assetsIndexFilePath)):
+    print("Cannot open asset index file:", assetsIndexFilePath)
+    print("Please enter its path manually in \"config.py\".")
+    exit(1)
+
+# 5. Open assets index json file.
+with open(assetsIndexFilePath) as assetsIndexFileData:
+    assetsIndex = json.load(assetsIndexFileData)
 
 
 # MARK: Main program
