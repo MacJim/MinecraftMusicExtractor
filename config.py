2e151acca7ac3d5dcbb2b857071c5ebbf263a0b2
Jim Shi <shiyilin2008@gmail.com>
2022-01-10 22:05:08 -0800

    [Minor] Code style: Updates to PEP 8 styles.

--- config.py
+++ config.py
@@ -3,21 +3,21 @@
 
 # 1. ".minecraft" (or "minecraft" on macOS) folder path.
 # If `None` or an empty string, will use the default Minecraft path.
-minecraftFolderPath = None
+minecraft_folder_path = None
 
 
 # 2. Music and sound effects export folder path.
 # If `None` or an empty string, all music and sound effects will be saved in the same folder of "main.py".
-exportFolderPath = None
+export_folder_path = None
 
 
 # 3. Overwrite existing files when exporting.
 # If `True`, this script will overwrite files at the destination folder.
 # Note: This script will NOT overwrite directories regardless of this value.
-shouldOverwriteExistingFiles = False
+should_overwrite_existing_files = False
 
 
 # 4. Minecraft assets index version.
 # If `None` or an empty string, this script will attempt to locate and extract music and sound effects from the LATEST Minecraft version on your computer.
-assetsIndexVersion = None
-# assetsIndexVersion = "1.12"
+assets_index_version = None
+# assets_index_version = "1.12"
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import platform
 import os
-import sys
 import distutils.version
 import json
 import shutil
@@ -8,7 +7,7 @@ import shutil
 import config
 
 
-# MARK: Helper functions.
+# MARK: Helper functions
 def getch():
     """Get a single character from user input. The user is not required to press ENTER.
     Source (many thanks): https://stackoverflow.com/questions/510357/python-read-a-single-character-from-the-user
@@ -20,7 +19,10 @@ def getch():
 
     except ModuleNotFoundError:
         # Works on UNIX.
-        import sys, tty, termios
+        import sys
+        import tty
+        import termios
+
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
         try:
@@ -41,41 +43,41 @@ print("Please run Minecraft at least once before running this script.\n")
 
 # MARK: Preparations.
 # 1. Get system platform.
-systemPlatform = platform.system()    # Linux, Darwin or Windows
+os_platform = platform.system()    # Linux, Darwin or Windows
 
 # 2. Obtain ".minecraft" folder path.
-minecraftFolderPath = config.minecraftFolderPath
-indexesFolderPath = ""
-objectsFolderPath = ""
+minecraft_folder_path = config.minecraft_folder_path
+indexes_folder_path = ""
+objects_folder_path = ""
 
-if ((minecraftFolderPath is None) or (minecraftFolderPath == "")):
-    # 2-1. Default ".minecraft" folder path.
-    if (systemPlatform == "Windows"):
-        appdataPath = os.getenv("APPDATA")
-        minecraftFolderPath = os.path.join(appdataPath, ".minecraft")
+if not minecraft_folder_path:
+    # 2-1. Find default ".minecraft" folder path.
+    if os_platform == "Windows":
+        appdata_path = os.getenv("APPDATA")
+        minecraft_folder_path = os.path.join(appdata_path, ".minecraft")
     else:
-        homeDirectoryPath = os.path.expanduser("~")
-        if (systemPlatform == "Darwin"):
-            minecraftFolderPath = os.path.join(homeDirectoryPath, "Library/Application Support/minecraft")    # No dot on macOS.
-        elif (systemPlatform == "Linux"):
-            minecraftFolderPath = os.path.join(homeDirectoryPath, ".minecraft")
+        home_directory_path = os.path.expanduser("~")
+        if os_platform == "Darwin":
+            minecraft_folder_path = os.path.join(home_directory_path, "Library/Application Support/minecraft")    # No dot on macOS.
+        elif os_platform == "Linux":
+            minecraft_folder_path = os.path.join(home_directory_path, ".minecraft")
 
-# 2-2. Makes sure that the default ".minecraft" folder and its "assets/indexes" & "assets/objects" subfolders exist.
-if (os.path.isdir(minecraftFolderPath)):
-    print("Minecraft folder found:", minecraftFolderPath)
+# 2-2. Check that the default ".minecraft" folder and its "assets/indexes" & "assets/objects" sub-folders exist.
+if os.path.isdir(minecraft_folder_path):
+    print("Minecraft folder found:", minecraft_folder_path)
 
-    indexesFolderPath = os.path.join(minecraftFolderPath, "assets/indexes")
-    objectsFolderPath = os.path.join(minecraftFolderPath, "assets/objects")
+    indexes_folder_path = os.path.join(minecraft_folder_path, "assets/indexes")
+    objects_folder_path = os.path.join(minecraft_folder_path, "assets/objects")
 
-    if (os.path.isdir(indexesFolderPath)):
-        print("Indexes folder found:", indexesFolderPath)
+    if os.path.isdir(indexes_folder_path):
+        print("Indexes folder found:", indexes_folder_path)
     else:
         print("Indexes folder not found!")
         print("Please run Minecraft at least once before running this script!")
         exit(1)
 
-    if (os.path.isdir(objectsFolderPath)):
-        print("Objects folder found:", objectsFolderPath)
+    if os.path.isdir(objects_folder_path):
+        print("Objects folder found:", objects_folder_path)
     else:
         print("Objects folder not found!")
         print("Please run Minecraft at least once before running this script!")
@@ -86,43 +88,43 @@ else:
     exit(1)
 
 # 3. Obtain export folder path.
-exportFolderPath = config.exportFolderPath
-if ((exportFolderPath is None) or (exportFolderPath == "")):
-    exportFolderPath = os.path.dirname(os.path.abspath(__file__))
+export_folder_path = config.export_folder_path
+if not export_folder_path:
+    export_folder_path = os.path.dirname(os.path.abspath(__file__))
 
 # 4. Parse "assets/indexes/1.x.json" file
-assetsIndexVersion = config.assetsIndexVersion
-if ((assetsIndexVersion is None) or (assetsIndexVersion == "")):
+assets_index_version = config.assets_index_version
+if not assets_index_version:
     # 4-1. Attempt to find the LATEST Minecraft asset version.
-    allVersions = []
+    all_versions = []
 
-    allFilenamesInIndexesFolder = os.listdir(indexesFolderPath)
-    for aFilename in allFilenamesInIndexesFolder:
-        if (aFilename.endswith(".json")):
-            allVersions.append(aFilename[0:-5])
+    all_filenames_in_indexes_folder = os.listdir(indexes_folder_path)
+    for filename in all_filenames_in_indexes_folder:
+        if filename.endswith(".json"):
+            all_versions.append(filename[0:-5])
 
-    if (len(allVersions) <= 0):
+    if not all_versions:
         print("No asset index found!")
         print("Please run Minecraft at least once before running this script!")
         exit(1)
     else:
-        assetsIndexVersion = allVersions[0]
-        for currentVersion in allVersions:
-            if (distutils.version.LooseVersion(assetsIndexVersion) < distutils.version.LooseVersion(currentVersion)):
-                assetsIndexVersion = currentVersion
-        print("Latest assets index found:", assetsIndexVersion)
+        assets_index_version = all_versions[0]
+        for current_version in all_versions:
+            if distutils.version.LooseVersion(assets_index_version) < distutils.version.LooseVersion(current_version):
+                assets_index_version = current_version
+        print("Latest assets index found:", assets_index_version)
 
 # 4-2. Make sure that the index file exists.
-assetsIndexFilePath = os.path.join(indexesFolderPath, assetsIndexVersion + ".json")
-if (not os.path.isfile(assetsIndexFilePath)):
-    print("Cannot open asset index file:", assetsIndexFilePath)
+assets_index_file_path = os.path.join(indexes_folder_path, assets_index_version + ".json")
+if not os.path.isfile(assets_index_file_path):
+    print("Cannot open asset index file:", assets_index_file_path)
     print("Please enter its path manually in \"config.py\".")
     exit(1)
 
 # 5. Open assets index json file.
-with open(assetsIndexFilePath) as assetsIndexFileData:
-    assetsIndex = json.load(assetsIndexFileData)
-    assetsIndex = assetsIndex["objects"]    # Don't forget this top-layered "object" key.
+with open(assets_index_file_path) as assetsIndexFileData:
+    assets_index = json.load(assetsIndexFileData)
+    assets_index = assets_index["objects"]    # Don't forget this top-layered "object" key.
 
 
 # MARK: Main program
@@ -132,87 +134,83 @@ print("2. Extract music & sound effects.")
 print("0. Exit")
 print("Press a key to select an option above.")
 
-while(True):
+while True:
     # Wait for a legal user input.
-    userInput = getch()
-    if (userInput == "0"):
+    user_input = getch()
+    if user_input == "0":
         exit(0)
-    elif ((userInput == "1") or (userInput == "2")):
+    elif (user_input == "1") or (user_input == "2"):
         break
     else:
         continue
 
 # 2. Filter items to export.
-crudeAssetsInformation = {}
-if (userInput == "1"):
-    for aKey, aValue in assetsIndex.items():
-        if (aKey.startswith("minecraft/sounds/music/")):
-            crudeAssetsInformation[aKey] = aValue
-elif (userInput == "2"):
-    for aKey, aValue in assetsIndex.items():
-        if (aKey.startswith("minecraft/sounds/")):
-            crudeAssetsInformation[aKey] = aValue
+crude_assets_information = {}
+if user_input == "1":
+    for k, v in assets_index.items():
+        if k.startswith("minecraft/sounds/music/"):
+            crude_assets_information[k] = v
+elif user_input == "2":
+    for k, v in assets_index.items():
+        if k.startswith("minecraft/sounds/"):
+            crude_assets_information[k] = v
 
 # 3. Calculate hashed path and export path.
-processedAssetsInformation = []
-for aKey, aValue in crudeAssetsInformation.items():
-    assetExportPath = os.path.join(exportFolderPath, aKey)
+processed_assets_information = []
+for k, v in crude_assets_information.items():
+    asset_export_path = os.path.join(export_folder_path, k)
 
-    assetHashedName = aValue["hash"]
-    assetHashedFolder = assetHashedName[0:2]
-    assetHashedPath = os.path.join(objectsFolderPath, assetHashedFolder, assetHashedName)
+    asset_hashed_name = v["hash"]
+    asset_hashed_folder = asset_hashed_name[0:2]
+    asset_hashed_path = os.path.join(objects_folder_path, asset_hashed_folder, asset_hashed_name)
 
-    processedAssetsInformation.append({
-        "exportPath": assetExportPath,
-        "hashedPath": assetHashedPath
+    processed_assets_information.append({
+        "exportPath": asset_export_path,
+        "hashedPath": asset_hashed_path
     })
 
 # 4. Copy files
-successfulCopies = []
+successful_copies = []
 # An IOError happened on this file. This might be caused by insufficient permission.
-failedCopies = []
+failed_copies = []
 # A file or directory exists at the export path.
-omittedCopies = []
+omitted_copies = []
 
-for aFileToCopy in processedAssetsInformation:
-    exportPath = aFileToCopy["exportPath"]
-    hashedPath = aFileToCopy["hashedPath"]
+for a_file_to_copy in processed_assets_information:
+    export_path = a_file_to_copy["exportPath"]
+    hashed_path = a_file_to_copy["hashedPath"]
 
     # 4-1. If a directory (not file) exists at `exportPath`, abort.
-    if (os.path.isdir(exportPath)):
-        omittedCopies.append(aFileToCopy)
+    if os.path.isdir(export_path):
+        omitted_copies.append(a_file_to_copy)
         continue
 
     # 4-2. Copy files.
-    if ((not os.path.isfile(exportPath)) or (config.shouldOverwriteExistingFiles)):
+    if (not os.path.isfile(export_path)) or config.should_overwrite_existing_files:
         try:
             # 4-3. Create the export directory if it does not exist.
-            if (not os.path.isdir(os.path.dirname(exportPath))):
-                os.makedirs(os.path.dirname(exportPath))
+            if not os.path.isdir(os.path.dirname(export_path)):
+                os.makedirs(os.path.dirname(export_path))
 
-            shutil.copyfile(hashedPath, exportPath)
-            successfulCopies.append(aFileToCopy)
+            shutil.copyfile(hashed_path, export_path)
+            successful_copies.append(a_file_to_copy)
         except:
-            failedCopies.append(aFileToCopy)
+            failed_copies.append(a_file_to_copy)
     else:
-        omittedCopies.append(aFileToCopy)
+        omitted_copies.append(a_file_to_copy)
 
 # 5. Show summary.
 print("\nCopy summary:")
-print("Successful copies:", len(successfulCopies))
-print("Failed copies:", len(failedCopies))
-print("Omitted copies:", len(omittedCopies))
+print("Successful copies:", len(successful_copies))
+print("Failed copies:", len(failed_copies))
+print("Omitted copies:", len(omitted_copies))
 
-if (len(failedCopies) > 0):
+if failed_copies:
     print("Errors occurred when copying certain files.")
     print("This might be caused by insufficient file permissions.")
-    print("Press any key to view them.")
-
-    sys.stdout.flush()
-    getch()
 
     print("Failed copies:")
-    for i in range(len(failedCopies)):
+    for i in range(len(failed_copies)):
         print(i)
-        print("Source:", failedCopies[i]["hashedPath"])
-        print("Destination:", failedCopies[i]["exportPath"])
+        print("Source:", failed_copies[i]["hashedPath"])
+        print("Destination:", failed_copies[i]["exportPath"])
